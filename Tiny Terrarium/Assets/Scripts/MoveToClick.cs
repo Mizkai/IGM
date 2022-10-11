50c1f39573b25471ea6df201cc087d3815ef3f45
wolfkeeper989 <adamskenyedab@gmail.com>
2022-10-11 08:29:17 -0400

    Proper Commit of Recent changes
    Sorry! I got the right repository this time. I fixed the move script. I also add the click script but it is not done yet!

--- Tiny Terrarium/Assets/Scripts/MoveToClick.cs
+++ Tiny Terrarium/Assets/Scripts/MoveToClick.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class MoveToClick : MonoBehaviour
 {
-    private Vector3 target;   
+    private Vector2 target;   
     
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if(Input.GetMouseButtonDown(0))
         {
-            target = new Vector3(mousepos.x, mousepos.y, mousepos.z);
+            target = new Vector2(mousepos.x, mousepos.y);
         }
 
-        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * 5f);
+        transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * 5f);
     }
 }
